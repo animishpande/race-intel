6255af6c04148fd84c3360aa58320b8e0825bec6
Animish <animishpande@gmail.com>
2025-07-06 10:29:38 +0530

    update build and webpack config for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
   output: {
-    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    path: path.resolve(__dirname, "build"),
   },
   plugins: [
     new HtmlWebpackPlugin({
